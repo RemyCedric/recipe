822e30ca16d0c7ef511fbc2f0d72252ea5f50101
RemyCedric <RemyCedric@outlook.com>
2021-02-23 21:53:03 +0100

    routing added to recipes and shopping-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  loadedFeature: string = 'recipe';
-
-  onNavigate(path: string) {
-    this.loadedFeature = path;
-  }
-}
+export class AppComponent {}
