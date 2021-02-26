e324e09fd7bff2fc4bf170d7ffc5f5661975dac7
RemyCedric <RemyCedric@outlook.com>
2021-02-26 21:22:28 +0100

    added authenticate page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth/auth.component';
 import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
@@ -20,7 +21,8 @@ const routes: Routes = [
     ],
     resolve: [RecipesResolverService],
   },
-  { path: 'shopping-list', component: ShoppingListComponent, children: [] },
+  { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'auth', component: AuthComponent },
 ];
 
 @NgModule({
