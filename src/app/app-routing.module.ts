96e05647feaa1cef47963118cbcfc4dfd9ef8bc7
RemyCedric <RemyCedric@outlook.com>
2021-03-03 17:04:32 +0100

    splitted app module with auth-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'auth', component: AuthComponent },
-];
+const routes: Routes = [{ path: '', redirectTo: '/recipes', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,22 +10,20 @@ import { RecipesModule } from './recipes/recipes.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AuthComponent } from './auth/auth.component';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent, //
     HeaderComponent,
-    AuthComponent,
   ],
   imports: [
     BrowserModule, //
-    FormsModule,
     HttpClientModule,
-    ReactiveFormsModule,
     AppRoutingModule,
+    AuthModule,
     RecipesModule,
     ShoppingListModule,
     SharedModule,
