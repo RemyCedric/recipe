be96320c865494213a8453525cdcb3b05df455b5
RemyCedric <RemyCedric@outlook.com>
2021-03-04 13:05:32 +0100

    added auth-action, updated auth-reducer

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,6 +1,8 @@
 import { Action } from '@ngrx/store';
 import { User } from '../user.model';
 
+import * as AuthActions from './auth.actions';
+
 export interface State {
   user: User | null;
 }
@@ -9,8 +11,18 @@ const initialState: State = {
   user: null,
 };
 
-export function authReducer(state = initialState, action: Action): State {
+export function authReducer(state = initialState, action: AuthActions.AuthActions): State {
   switch (action.type) {
+    case AuthActions.LOGIN:
+      return {
+        ...state, //
+        user: action.payload,
+      };
+    case AuthActions.LOGOUT:
+      return {
+        ...state, //
+        user: null,
+      };
     default:
       return state;
   }
